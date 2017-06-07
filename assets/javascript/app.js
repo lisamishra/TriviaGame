245e5f3c1585e9d0383464fc56c213872a351fae
Lisa Mishra <lisamishra@Lisas-MacBook-Pro.local>
2017-06-06 22:01:03 -0500

    Add new home

---
+++ assets/javascript/app.js
@@ -0,0 +1,84 @@
+//Quiz Variables
+
+var questionCount = 0;
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+
+//show and hide elements
+
+function show(){
+	var reveal = document.getElementbyID('quizSection').style.display ='block';
+}
+
+function hide(){
+	var mask = document.getElementbyID('quiz').style.display ='none';
+}
+
+
+//answers
+
+ 	if(document.getElementById('1d').checked){
+      correct++;
+    } else if (document.getElementById('1a').checked ||document.getElementById('1b').checked  
+      || document.getElementById('1c').checked){
+      incorrect++;
+    } else {
+      unanswered++;
+    }
+
+    if(document.getElementById('2a').checked){
+      correct++;
+    } else if (document.getElementById('2b').checked ||document.getElementById('2c').checked  
+      || document.getElementById('2d').checked){
+      incorrect++;
+    } else {
+      unanswered++;
+    }
+
+    if(document.getElementById('3c').checked){
+      correct++;
+    } else if (document.getElementById('3a').checked ||document.getElementById('3b').checked  
+      || document.getElementById('3d').checked){
+      incorrect++;
+    } else {
+      unanswered++;
+    }
+
+    if(document.getElementById('4c').checked){
+      correct++;
+    } else if (document.getElementById('4a').checked ||document.getElementById('4b').checked  
+      || document.getElementById('4d').checked){
+      incorrect++;
+    } else {
+      unanswered++;
+    }
+
+    if(document.getElementById('5c').checked){
+      correct++;
+    } else if (document.getElementById('5a').checked ||document.getElementById('5b').checked  
+      || document.getElementById('5d').checked){
+      incorrect++;
+    } else {
+      unanswered++;
+    }
+
+    if(document.getElementById('6d').checked){
+      correct++;
+    } else if (document.getElementById('6a').checked ||document.getElementById('6b').checked  
+      || document.getElementById('6c').checked){
+      incorrect++;
+    } else {
+      unanswered++;
+    }
+
+//results
+
+var feedback = $("div");
+      feedback.addClass("score");
+      $("#score").append(fb);
+      feedback.html("You did aight, but you ain't love Kanye like Kanye." + "<br>" 
+        + "Correct answers:  " + correct + "<br>"
+        + "Incorrect answers: " + incorrect + "<br>"
+        + "Unanswered questions:  " + unanswered + "<br>");
+
